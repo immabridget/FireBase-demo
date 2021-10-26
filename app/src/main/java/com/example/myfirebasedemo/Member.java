d0ad6a31f671b5ab28dcbcb76ba4582722eb7101
Immeculate <immabridg@gmail.com>
2021-10-26 15:16:28 +0300

    inserting and retreiving data in firebase

---
+++ app/src/main/java/com/example/myfirebasedemo/Member.java
@@ -0,0 +1,47 @@
+package com.example.myfirebasedemo;
+
+public class Member {
+    private String FirstName,LastName,phone,mail;
+
+    public Member() {
+    }
+
+    public Member(String firstName, String lastName, String phone, String mail) {
+        FirstName = firstName;
+        LastName = lastName;
+        this.phone = phone;
+        this.mail = mail;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
